c175df0830d2e8a8ce01c38b935e093b495f4fad
Vigen <vigen.khachatryan.00@gmail.com>
2022-02-21 04:37:58 +0400

    Added React Draft Wysiwyg editor integration

---
+++ front/src/components/EditorComponent/EditorComponent.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { EditorState, convertToRaw, ContentState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "./EditorComponent.css"
+
+
+const toolbar = {
+  options: ['history', 'inline', 'blockType', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link'],
+  inline: {
+    options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace'],
+  },
+  list: {
+    options: ['unordered', 'ordered']
+  },
+  fontFamily: {
+    options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana']
+  }
+}
+
+export function EditorConvertToHTML (props) {
+  const [state, setState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (editorState) => {
+    setState(editorState);
+  };
+
+  return (
+    <div className={'editor'}>
+      <Editor
+        toolbar={toolbar}
+        editorState={state}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+        onEditorStateChange={onEditorStateChange}
+      />
+      <textarea
+        className={'html-area'}
+        // disabled
+        value={draftToHtml(convertToRaw(state.getCurrentContent()))}
+      />
+    </div>
+  );
+}
+
+// export class EditorConvertToHTML extends Component {
+//   constructor(props) {
+//     super(props);
+//     const html = '<p>Hey this <strong>editor</strong> rocks 😀</p>';
+//     const contentBlock = htmlToDraft(html);
+//     if (contentBlock) {
+//       const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+//       const editorState = EditorState.createWithContent(contentState);
+//       this.state = {
+//         editorState: EditorState.createEmpty()
+//       };
+//     }
+//   }
+
+//   onEditorStateChange = (editorState) => {
+//     this.setState({
+//       editorState,
+//     });
+//   };
+
+//   render() {
+//     const { editorState } = this.state;
+//     return (
+//       <div className={'editor'}>
+//         <Editor
+//           toolbar={toolbar}
+//           editorState={editorState}
+//           wrapperClassName="wrapper-class"
+//           editorClassName="editor-class"
+//           onEditorStateChange={this.onEditorStateChange}
+//         />
+//         {/* <textarea
+//           disabled
+//           value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+//         /> */}
+//       </div>
+//     );
+//   }
+// }
